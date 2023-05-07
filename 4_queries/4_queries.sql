f2857f3df040acdcbd6299a46cc1c15a6b589a1e
Kryxsoo <MattCharmZ@gmail.com>
2023-05-07 06:42:52 -0700

    Add 4_queries

---
+++ 4_queries/4_queries.sql
@@ -0,0 +1,46 @@
+SELECT count(assistance_requests.*) as total_assistances, teachers.name
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+WHERE name = 'Waylon Boehm'
+GROUP BY teachers.name;
+
+SELECT count(assistance_requests.*) as total_assistances, students.name
+FROM assistance_requests
+JOIN students ON students.id = student_id
+WHERE name = 'Elliot Dickinson'
+GROUP BY students.name;
+
+SELECT teachers.name as teacher, students.name as student, assignments.name as assignment, (completed_at-started_at) as duration
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN assignments ON assignments.id = assignment_id
+ORDER BY duration;
+
+SELECT cohorts.name, avg(completed_at - started_at) as average_assistance_time
+FROM assistance_requests 
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohorts.name
+ORDER BY average_assistance_time;
+
+SELECT cohorts.name, avg(completed_at - started_at) as average_assistance_time
+FROM assistance_requests 
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = cohort_id
+GROUP BY cohorts.name
+ORDER BY average_assistance_time;
+
+--Nested
+SELECT avg (total_duration) as average_total_duration
+FROM (
+  SELECT cohorts.name as cohort, sum(completed_at-started_at) as total_duration
+  FROM assistance_requests
+  JOIN students ON students.id = student_id
+  JOIN cohorts on cohorts.id = cohort_id
+  GROUP BY cohorts.name
+  ORDER BY total_duration
+) as total_durations;
+
+SELECT id, name, day, chapter
+FROM assignments
